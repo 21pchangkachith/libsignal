ed02b6f424fa0c967ba97c5c68f71889d249f3d3
21pchangkachith <67136281+21pchangkachith@users.noreply.github.com>
2026-05-26 15:05:39 -0700

    fix vk using edward-ristretto fusion 50% of the time

--- rust/protocol/src/ratchet.rs
+++ rust/protocol/src/ratchet.rs
@@ -19,6 +19,7 @@ mod keys;
 mod params;
 
 use curve25519_dalek::constants::ED25519_BASEPOINT_POINT;
+use curve25519_dalek::edwards::CompressedEdwardsY;
 use libsignal_core::derive_arrays;
 use rand::{CryptoRng, Rng};
 
@@ -125,7 +126,7 @@ pub fn hash_generic_zp(input: &[u8]) -> Scalar {
     Scalar::from_bytes_mod_order_wide(&hash.into())
 }
 
-pub fn hash_fs(vk: &RistrettoPoint, x: &[u8], h: &RistrettoPoint, h_prime: &RistrettoPoint, eta: &RistrettoPoint, eta_prime: &RistrettoPoint) -> Scalar {
+pub fn hash_fs(vk: &EdwardsPoint, x: &[u8], h: &EdwardsPoint, h_prime: &RistrettoPoint, eta: &EdwardsPoint, eta_prime: &RistrettoPoint) -> Scalar {
     let mut bytes = Vec::new();
     //bytes.extend(&(vk.len() as u64).to_le_bytes());
     bytes.extend(vk.compress().as_bytes());
@@ -147,25 +148,40 @@ fn hash_to_g(domain_sep: &[u8], input: &[u8]) -> RistrettoPoint {
     RistrettoPoint::from_hash(hasher)
 }
 
-pub fn hash_i(vk: &RistrettoPoint , x: &[u8]) -> RistrettoPoint {
-    let compressed = vk.compress();
-    let compressed_vecu8 = compressed.as_bytes().to_vec();
-    hash_to_g(b"hash_i", &encode(&compressed_vecu8, x))
+fn hash_to_g_edwards(domain_sep: &[u8], input: &[u8]) -> EdwardsPoint {
+    let mut bytes = Vec::new();
+    //bytes.extend(&(vk.len() as u64).to_le_bytes());
+    bytes.extend(domain_sep);
+    bytes.extend(input);
+
+    let bytes: [u8; 32] = Sha512::digest(&bytes).as_slice()[..32].try_into().unwrap();
+    EdwardsPoint::mul_base_clamped(bytes)
+    // let pt = CompressedEdwardsY::from_slice(&bytes)
+    //     .unwrap()
+    //     .decompress()
+    //     .unwrap();
+    // pt
 }
 
-pub fn hash_a(vk: &RistrettoPoint, x: &[u8]) -> RistrettoPoint {
+pub fn hash_i(vk: &EdwardsPoint , x: &[u8]) -> EdwardsPoint {
+    let compressed = vk.compress();
+    let compressed_vecu8 = compressed.as_bytes().to_vec();
+    hash_to_g_edwards(b"hash_i", &encode(&compressed_vecu8, x))
+}
+
+pub fn hash_a(vk: &EdwardsPoint, x: &[u8]) -> RistrettoPoint {
     let compressed = vk.compress();
     let compressed_vecu8 = compressed.as_bytes().to_vec();
     hash_to_g(b"hash_a", &encode(&compressed_vecu8, x))
 }
 
-pub fn hash_b(vk: &RistrettoPoint, x: &[u8]) -> RistrettoPoint {
+pub fn hash_b(vk: &EdwardsPoint, x: &[u8]) -> RistrettoPoint {
     let compressed = vk.compress();
     let compressed_vecu8 = compressed.as_bytes().to_vec();
     hash_to_g(b"hash_b", &encode(&compressed_vecu8, x))
 }
 
-pub fn hash_o(point: &RistrettoPoint) -> Vec<u8> {
+pub fn hash_o(point: &EdwardsPoint) -> Vec<u8> {
     // Compress the RistrettoPoint to a canonical 32-byte representation
     let compressed = point.compress();
 
@@ -213,7 +229,7 @@ pub(crate) fn initialize_alice_session<R: Rng + CryptoRng>(
 
     //step 0: parameters
     let vk; //otpk_bob,i 
-    let g = generator_g();
+    let g = ED25519_BASEPOINT_POINT;
     let k;
 
     // Optional Alice's eph key * Bob's otpk
@@ -223,7 +239,7 @@ pub(crate) fn initialize_alice_session<R: Rng + CryptoRng>(
         k = hash_generic_zp(their_one_time_prekey.public_key_bytes());
         //vk = g * k;
         let bytes: [u8; 32] = their_one_time_prekey.public_key_bytes().try_into().unwrap();
-        vk = RistrettoPoint::from_uniform_bytes_single_elligator(&bytes);
+        vk = MontgomeryPoint(bytes).to_edwards(0).unwrap();
         // let mont_pt = MontgomeryPoint(bytes);
         // let ed_pt = mont_pt.to_edwards(0).unwrap();
         // ed_pt.
@@ -233,7 +249,7 @@ pub(crate) fn initialize_alice_session<R: Rng + CryptoRng>(
         k = hash_generic_zp(parameters.their_signed_pre_key().public_key_bytes());
         //vk = g * k;
         let bytes: [u8; 32] = parameters.their_signed_pre_key().public_key_bytes().try_into().unwrap();
-        vk = RistrettoPoint::from_uniform_bytes_single_elligator(&bytes);
+        vk = MontgomeryPoint(bytes).to_edwards(0).unwrap();
     }
     x.extend_from_slice(parameters.their_signed_pre_key().public_key_bytes());
     let mut alice_sas_contribution_salt = [0u8; 3];
@@ -254,14 +270,14 @@ pub(crate) fn initialize_alice_session<R: Rng + CryptoRng>(
     //step 1, page 11, preverify
     let alpha = sample_random_zp(csprng);
     let beta = sample_random_zp(csprng);
-    let h = alpha * g + beta * hash_i(&vk, &x);    
+    let h = ((alpha * g) + (beta * hash_i(&vk, &x)));    
     let hprime = alpha * hash_a(&vk, &x) + beta * hash_b(&vk, &x);
 
     //step 2
     let r1 = sample_random_zp(csprng);
     let r2 = sample_random_zp(csprng);
     // scalar * point = point to the power of scaler in paper
-    let eta = (g * r1) + (hash_i(&vk, &x) * r2);
+    let eta = ((g * r1) + (hash_i(&vk, &x) * r2));
     let etaprime = (hash_a(&vk, &x) * r1) + (hash_b(&vk, &x) * r2);
     let c = hash_fs(&vk, &x, &h, &hprime, &eta, &etaprime);
     //s values should be mod p
@@ -392,7 +408,7 @@ pub(crate) fn initialize_bob_session(
 
     //step 0: "retrieve" parameters k, vk
     // fresh genning them for rapid dev right now
-    let g = generator_g();
+    let g = ED25519_BASEPOINT_POINT;
     let k ;
     let vk ;
     let mut x = Vec::with_capacity(32 * 6);
@@ -420,7 +436,7 @@ pub(crate) fn initialize_bob_session(
         k = Scalar::from_bytes_mod_order(arr);
         //Scalar::from_bytes_mod_order(arr);
         let bytes: [u8; 32] = our_one_time_pre_key_pair.public_key.public_key_bytes().try_into().unwrap();
-        vk = RistrettoPoint::from_uniform_bytes_single_elligator(&bytes);
+        vk = MontgomeryPoint(bytes).to_edwards(0).unwrap();
         //  CompressedRistretto::from_slice(&bytes)
         //     .unwrap()
         //     .decompress()
@@ -428,18 +444,22 @@ pub(crate) fn initialize_bob_session(
         // let priva = our_one_time_pre_key_pair.private_key;
         // let unwra = priva.public_key().unwrap();
         // let puba_byte = unwra.public_key_bytes();
-        let idealvk = g * k;
-        let testvk = ED25519_BASEPOINT_POINT * k;
-        let test_calcvk = EdwardsPoint::mul_base_clamped(bytes);
-        let test2_calcvk = MontgomeryPoint(bytes).to_edwards(0).unwrap();
-        // log::info!("puba top {:?}", puba_byte);
-        // log::info!("real pub {:?}", bytes);
-        log::info!("ideal vk {:?}", idealvk.compress().to_bytes());
-        log::info!("real vk {:?}", vk.compress().to_bytes());
-        log::info!("bytes of vk {:?}", bytes);
-        log::info!("test vk {:?}", testvk.compress().to_bytes());
-        log::info!("test calc vk {:?}", test_calcvk.compress().to_bytes());
-        log::info!("test2 calc vk {:?}", test2_calcvk.compress().to_bytes());
+        // let idealvk = RISTRETTO_BASEPOINT_POINT * k;
+        // let testvk = ED25519_BASEPOINT_POINT * k;
+        // let test2_calcvk = MontgomeryPoint(bytes).to_edwards(0).unwrap();
+        // let test2_compressed = test2_calcvk.compress();
+        // //let test_calcvk = EdwardsPoint::mul_base_clamped(bytes);
+
+        // // log::info!("puba top {:?}", puba_byte);
+        // // log::info!("real pub {:?}", bytes);
+        // log::info!("ideal vk {:?}", idealvk.compress().to_bytes());
+        // log::info!("real vk {:?}", vk.compress().to_bytes());
+        // log::info!("bytes of vk {:?}", bytes);
+        // log::info!("test vk {:?}", testvk.compress().to_bytes());
+        // //log::info!("test calc vk {:?}", test_calcvk.compress().to_bytes());
+        // log::info!("test2 calc vk {:?}", test2_calcvk.compress().to_bytes());
+        // log::info!("test2 calc vk compressed {:?}", test2_compressed.to_bytes());
+        //assert_eq!(test2_calcvk, testvk);
         // k=hash_generic_zp(our_one_time_pre_key_pair.public_key.public_key_bytes());
         // vk = g * k;
         
@@ -451,7 +471,7 @@ pub(crate) fn initialize_bob_session(
             .expect("at least 32 bytes required");
         k = Scalar::from_bytes_mod_order(arr);
         let bytes: [u8; 32] = parameters.our_signed_pre_key_pair().public_key.public_key_bytes().try_into().unwrap();
-        vk = RistrettoPoint::from_uniform_bytes_single_elligator(&bytes);
+        vk = MontgomeryPoint(bytes).to_edwards(0).unwrap();
         let priva = parameters.our_signed_pre_key_pair().private_key;
         let unwra = priva.public_key().unwrap();
         let puba_byte = unwra.public_key_bytes();
@@ -491,7 +511,7 @@ pub(crate) fn initialize_bob_session(
             (h, hprime, (c, (s1, s2))),
             their_contrib_salt
         ): (
-            (RistrettoPoint, RistrettoPoint, (Scalar, (Scalar, Scalar))),
+            (EdwardsPoint, RistrettoPoint, (Scalar, (Scalar, Scalar))),
             Vec<u8>
         ) = bincode::deserialize(&bytes).unwrap();
         // Step 1, parse vars
@@ -505,7 +525,7 @@ pub(crate) fn initialize_bob_session(
 
         // η = g^s1 * Hi(vk,x)^s2 * h^c
         // η' = Ha(vk,x)^s1 * Hb(vk,x)^s2 * h'^c
-        let eta = (g * (s1)) + (hi * (s2)) + h * (c);
+        let eta = (((g * (s1)) + (hi * (s2))) + (h * (c)));
         let etaprime = (ha * (s1)) + (hb * (s2)) + (hprime * (c));
 
         let computed_c = hash_fs(&vk, &x, &h, &hprime, &eta, &etaprime);
@@ -517,9 +537,9 @@ pub(crate) fn initialize_bob_session(
         } 
 
         // Step 3
-        let w = hi*(k);
+        let w = (hi*(k));
         let z = hash_o(&w); //the sas?
-        let v = h * (k);
+        let v = (h * (k));
         let pi = (w, v);
 
         // Step 4
@@ -637,12 +657,12 @@ pub fn pvrf_verify_from_session_data(
         .ok_or_else(|| SignalProtocolError::InvalidArgument("invalid beta scalar".to_string()))?;
 
     // Parse w and v points
-    let w = CompressedRistretto::from_slice(w_bytes)
+    let w = CompressedEdwardsY::from_slice(w_bytes)
         .map_err(|_| SignalProtocolError::InvalidArgument("invalid w slice".to_string()))?
         .decompress()
         .ok_or_else(|| SignalProtocolError::InvalidArgument("w decompression failed".to_string()))?;
 
-    let v = CompressedRistretto::from_slice(v_bytes)
+    let v = CompressedEdwardsY::from_slice(v_bytes)
         .map_err(|_| SignalProtocolError::InvalidArgument("invalid v slice".to_string()))?
         .decompress()
         .ok_or_else(|| SignalProtocolError::InvalidArgument("v decompression failed".to_string()))?;
@@ -656,11 +676,16 @@ pub fn pvrf_verify_from_session_data(
     // Reconstruct vk as a RistrettoPoint
     log::info!("what is vk {:?} bytes", vk_bytes);
     log::info!("what is v {:?} bytes", v_bytes);
-    let vk_point = CompressedRistretto::from_slice(vk_bytes)
+    let vk_point = CompressedEdwardsY::from_slice(vk_bytes)
         .map_err(|_| SignalProtocolError::InvalidArgument("invalid vk slice".to_string()))?
         .decompress()
         .ok_or_else(|| SignalProtocolError::InvalidArgument("vk decompression failed".to_string()))?;
-    log::info!("whyyyy");
+    log::info!("whyyyy {:?} bytes", vk_point.compress().as_bytes());
+    // CompressedRistretto::from_slice(vk_bytes)
+    //     .map_err(|_| SignalProtocolError::InvalidArgument("invalid vk slice".to_string()))?
+    //     .decompress()
+    //     .ok_or_else(|| SignalProtocolError::InvalidArgument("vk decompression failed".to_string()))?;
+    // log::info!("whyyyy");
 
 
 
@@ -668,8 +693,76 @@ pub fn pvrf_verify_from_session_data(
 
     // v = vk^alpha * w^beta
     let expected_v = (vk_point * alpha) + (w * beta);
-    let ok = v == expected_v;
+    let ok = v.compress().as_bytes() == expected_v.compress().as_bytes();
+    log::info!("what v is {:?} bytes", v.compress().as_bytes());
     log::info!("what calculated v is {:?} bytes", expected_v.compress().as_bytes());
+    if ok {
+        log::info!("PVRF VERIFY SUCCESS z: {:?}", z);
+    } else {
+        log::error!("PVRF VERIFY FAILED: v != expected_v");
+    }
+
+    Ok((ok, z))
+}
+
+
+
+
+pub fn pvrf_verify_raw(
+    vk: EdwardsPoint,
+    x_bytes: &[u8],
+    alpha_bytes: &[u8],  // 32-byte LE scalar
+    beta_bytes: &[u8],   // 32-byte LE scalar
+    w: EdwardsPoint,
+    v: EdwardsPoint
+) -> Result<(bool, Vec<u8>)> {
+
+    // Parse alpha and beta scalars
+    let alpha_arr: [u8; 32] = alpha_bytes.try_into().map_err(|_|
+        SignalProtocolError::InvalidArgument("alpha must be 32 bytes".to_string()))?;
+    let beta_arr: [u8; 32] = beta_bytes.try_into().map_err(|_|
+        SignalProtocolError::InvalidArgument("beta must be 32 bytes".to_string()))?;
+
+    let alpha = Option::<Scalar>::from(Scalar::from_canonical_bytes(alpha_arr))
+        .ok_or_else(|| SignalProtocolError::InvalidArgument("invalid alpha scalar".to_string()))?;
+    let beta = Option::<Scalar>::from(Scalar::from_canonical_bytes(beta_arr))
+        .ok_or_else(|| SignalProtocolError::InvalidArgument("invalid beta scalar".to_string()))?;
+    log::info!("the v bytes is {:?}", v);
+    //let vk = vk_bytes.to_vec();
+    //let x = x_bytes.to_vec();
+
+    // z = Ho(w)
+    let z = hash_o(&w);
+
+    // Reconstruct vk as a RistrettoPoint
+    log::info!("what is vk {:?} bytes", vk);
+    let vk_point = vk;
+    log::info!("whyyyy {:?} bytes", vk_point.compress().as_bytes());
+    // CompressedRistretto::from_slice(vk_bytes)
+    //     .map_err(|_| SignalProtocolError::InvalidArgument("invalid vk slice".to_string()))?
+    //     .decompress()
+    //     .ok_or_else(|| SignalProtocolError::InvalidArgument("vk decompression failed".to_string()))?;
+    // log::info!("whyyyy");
+
+
+    // v = vk^alpha * w^beta
+    let expected_v = (vk_point * alpha) + (w * beta);
+    let v = v;
+    let mont_v = v.to_montgomery();
+    let mont_expected_v = expected_v.to_montgomery();
+    log::info!("montgomery ok {:?}", mont_v == mont_expected_v);
+    log::info!("what is mont v {:?} bytes", mont_v.to_bytes());
+    log::info!("what is mont expected v {:?} bytes", mont_expected_v.to_bytes());
+    let ok = v.compress().as_bytes() == expected_v.compress().as_bytes() || mont_v == mont_expected_v;
+    log::info!("what v is {:?} bytes", v.compress().as_bytes());
+    log::info!("what calculated v is {:?} bytes", expected_v.compress().as_bytes());
+    log::info!("compressed ok {:?}, uncompressed ok {:?} ", v.compress().as_bytes() == expected_v.compress().as_bytes(), v == expected_v);
+    let v_cofa = v.mul_by_cofactor();
+    let expected_v_cofa = expected_v.mul_by_cofactor();
+    log::info!("what v cofa is {:?} bytes", v_cofa.compress().as_bytes());
+    log::info!("what calculated v cofa is {:?} bytes", expected_v_cofa.compress().as_bytes());
+    log::info!("cofa eq {:?}", v_cofa == expected_v_cofa);
+    log::info!("full v {:?}, full expected_v {:?} ", v, expected_v);
     if ok {
         log::info!("PVRF VERIFY SUCCESS z: {:?}", z);
     } else {
